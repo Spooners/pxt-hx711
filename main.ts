4b90d31ae8c919490b02e79be276711da218cd97
Spooners <31957642+Spooners@users.noreply.github.com>
2018-12-28 14:53:33 +0100

    Added some teste

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+namespace hx711{
+    /**
+     * This is a statement block
+     */
+    //% block
+    export function statement() {
+
+    }
+
+    /**
+     * A function that returns a non-void argument generates a reporter block (oval shape).
+     */
+    //% block
+    export function expression(): number {
+        return 0;
+    }
+}
